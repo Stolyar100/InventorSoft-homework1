a92c04f2031cefdbfd6671cb0f0d5bdc114e9c48
Oleh Marchuk <olehmarchukdev@gmail.com>
2021-08-27 20:16:32 +0300

    create method to find area of all possible submatrix

--- src/scripts/services/task-service.mjs
+++ src/scripts/services/task-service.mjs
@@ -1,10 +1,14 @@
 class TaskService {
   calcMaxSubmatrixArea(matrix) {
-    console.log(matrix)
-    const matrixOfColumnLengths = matrix.map((row, rowIndex, matrix) =>
-      row.map((rowItem, rowItemIndex) => this.countColumnLength(matrix, rowIndex, rowItemIndex)),
+    const columnLengthsMatrix = matrix.map((row, rowIndex, matrix) =>
+      row.map((rowItem, rowItemIndex) =>
+        this.countColumnLength(matrix, rowIndex, rowItemIndex),
+      ),
     )
-  console.log(matrixOfColumnLengths)
+    const submatrixAreasMatrix = columnLengthsMatrix.map(
+      this.calcSubmatrixAreasCombination,
+    )
+    console.log(submatrixAreasMatrix)
   }
 
   countColumnLength(matrix, rowIndex, columnIndex) {
@@ -17,6 +21,26 @@ class TaskService {
       }
     }
     return columnLengths
+  }
+
+  calcSubmatrixAreasCombination(arrayOfColumnLenghts) {
+    let arrayOfsubmatrixAreas = []
+    const maxColumnLength = Math.max(...arrayOfColumnLenghts)
+    for (let i = 0; i < maxColumnLength + 1; i++) {
+      let elementSeriesLength = 0
+      for (let j = 0; j < arrayOfColumnLenghts.length; j++) {
+        if (arrayOfColumnLenghts[j] >= i) {
+          elementSeriesLength += 1
+        } else {
+          arrayOfsubmatrixAreas.push(i * elementSeriesLength)
+          elementSeriesLength = 0
+        }
+      }
+      if (elementSeriesLength !== 1) {
+        arrayOfsubmatrixAreas.push(i * elementSeriesLength)
+      }
+    }
+    return arrayOfsubmatrixAreas
   }
 }
 
