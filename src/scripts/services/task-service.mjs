1a24307a27794d42d5631b3e0c5350bcf092fa33
Oleh Marchuk <olehmarchukdev@gmail.com>
2021-08-27 12:05:39 +0300

    Separate counting code to countColumnLength method

--- src/scripts/services/task-service.mjs
+++ src/scripts/services/task-service.mjs
@@ -2,19 +2,21 @@ class TaskService {
   calcMaxSubmatrixArea(matrix) {
     console.log(matrix)
     const matrixOfColumnLengths = matrix.map((row, rowIndex, matrix) =>
-      row.map((rowItem, rowItemIndex) => {
-        let columnLengths = 0
-        for (let i = rowIndex; i < matrix.length; i++) {
-          if (matrix[i][rowItemIndex] === 1) {
-            columnLengths += 1
-          } else {
-            break
-          }
-        }
-        return columnLengths
-      }),
+      row.map((rowItem, rowItemIndex) => this.countColumnLength(matrix, rowIndex, rowItemIndex)),
     )
-    console.log(matrixOfColumnLengths)
+  console.log(matrixOfColumnLengths)
+  }
+
+  countColumnLength(matrix, rowIndex, columnIndex) {
+    let columnLengths = 0
+    for (let i = rowIndex; i < matrix.length; i++) {
+      if (matrix[i][columnIndex] === 1) {
+        columnLengths += 1
+      } else {
+        break
+      }
+    }
+    return columnLengths
   }
 }
 
